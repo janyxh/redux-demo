ff6fe30f92ce0b31b9427fb8202c2b0ce0995b1d
meihua.xie <janyxh119@163.com>
2019-11-11 18:30:52 +0800

    添加redux的一些操作

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ function App() {
       <Link to="/">
           <Button type="primary">List</Button>
       </Link>
+      <Link to="/context">
+          <Button type="primary">Context</Button>
+      </Link>
+      <Link to="/prop">
+          <Button type="primary">Prop</Button>
+      </Link>
       <Routes />
     </div>
   );
--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from "react-router-dom";
-import List from './Page/List';
-import About from './Page/About';
+
+import List from "./Page/List"
+const About = lazy(() => import('./Page/About'));
+const AppContext = lazy(() => import('./Page/Context'));
+const Appprop = lazy(() => import('./Page/Prop'));
 
 class Routes extends React.Component {
     render() {
         return (
-            <Switch>
-                <Route exact path="/" component={List} />
-                <Route path="/about" component={About} />
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route exact path="/" component={List} />
+                    <Route path="/about" component={About} />
+                    <Route path="/context" component={AppContext} />
+                    <Route path="/prop" component={Appprop} />
+                </Switch>
+            </Suspense >
         )
     }
 }
